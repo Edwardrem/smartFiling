d160b12a6aebce807b9de422414590419ac5002b
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2024-03-01 11:22:17 +0200

    successfully  created the admin panel for the Prince Shipping System

--- prince_shipping_system/shipping_system/admin.py
+++ prince_shipping_system/shipping_system/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Importer, BillOfEntry, InternalDocument
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'role')
+
+@admin.register(Importer)
+class ImporterAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(BillOfEntry)
+class BillOfEntryAdmin(admin.ModelAdmin):
+    list_display = ('importer', 'entry_number', 'invoice_reference', 'description')
+
+@admin.register(InternalDocument)
+class InternalDocumentAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'document_name', 'description')
